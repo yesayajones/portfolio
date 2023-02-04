914cf93c126f552cb89bfb466acd0bcaf26cc6e7
yesayajones <yesayajones@gmail.com>
2023-02-04 12:22:11 +0200

    feat: Add technology section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,61 @@
 import React from 'react';
 import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import {
+	Section,
+	SectionDivider,
+	SectionText,
+	SectionTitle,
+} from '../../styles/GlobalComponents';
+import {
+	List,
+	ListContainer,
+	ListItem,
+	ListParagraph,
+	ListTitle,
+} from './TechnologiesStyles';
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+const Technologies = () => (
+	<Section id='tech'>
+		<SectionDivider />
+		<br />
+		<SectionTitle>Technologies</SectionTitle>
+		<SectionText>
+			I have worked with many technologies in the web development world. Fron
+			frontend to Design.
+		</SectionText>
+		<List>
+			<ListItem>
+				<DiReact size='3rem' />
+				<ListContainer>
+					<ListTitle>Front-end</ListTitle>
+					<ListParagraph>
+						Experience with <br />
+						React.js
+					</ListParagraph>
+				</ListContainer>
+			</ListItem>
+			<ListItem>
+				<DiFirebase size='3rem' />
+				<ListContainer>
+					<ListTitle>Back-end</ListTitle>
+					<ListParagraph>
+						Experience with <br />
+						Node.js and Databases
+					</ListParagraph>
+				</ListContainer>
+			</ListItem>
+			<ListItem>
+				<DiZend size='3rem' />
+				<ListContainer>
+					<ListTitle>UI/UX Design</ListTitle>
+					<ListParagraph>
+						Experience with <br />
+						tools like Figma
+					</ListParagraph>
+				</ListContainer>
+			</ListItem>
+		</List>
+	</Section>
 );
 
 export default Technologies;
