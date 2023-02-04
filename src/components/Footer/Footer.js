ef23b9f3e08f658e80c2f3edab5386f2deccd6e4
yesayajones <yesayajones@gmail.com>
2023-02-04 17:25:55 +0200

    feat: Add Accomplishments and footer section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,14 +2,29 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 
 import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import {
+	CompanyContainer,
+	FooterWrapper,
+	LinkColumn,
+	LinkItem,
+	LinkList,
+	LinkTitle,
+	Slogan,
+	SocialContainer,
+	SocialIconsContainer,
+} from './FooterStyles';
 
 const Footer = () => {
-  return (
-    <div>
-      Footer
-    </div>
-  );
+	return (
+		<FooterWrapper>
+			<LinkList>
+				<LinkColumn>
+					<LinkTitle>Call</LinkTitle>
+					<LinkItem href='tel:+44777777777'>+447-777-77777</LinkItem>
+				</LinkColumn>
+			</LinkList>
+		</FooterWrapper>
+	);
 };
 
 export default Footer;
