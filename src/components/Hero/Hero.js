8128b6ebd97850da7a92014321b2c5be951bc177
yesayajones <yesayajones@gmail.com>
2023-02-04 11:35:08 +0200

    feat: add hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {
+	Section,
+	SectionText,
+	SectionTitle,
+} from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+	<Section row nopadding>
+		<LeftSection>
+			<SectionTitle main center>
+				Welcom To <br />
+				My Personal Portfolio
+			</SectionTitle>
+			<SectionText>
+				I am a front-end web developer with React experience based in Zimbabwe.
+				I offer differenct front-end development services.
+			</SectionText>
+			<Button onClick={() => (window.location = 'https://google.com')}>
+				Learn More
+			</Button>
+		</LeftSection>
+	</Section>
 );
 
 export default Hero;
