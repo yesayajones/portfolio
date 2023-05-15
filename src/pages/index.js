a78b7c36dea244ccebc8c7098810df58bdafe466
yesayajones <yesayajones@gmail.com>
2023-05-15 04:12:28 +0200

    chore: Add necessary modules

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
+//Import necessary components
 import Acomplishments from '../components/Acomplishments/Acomplishments';
 import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation';
 import Hero from '../components/Hero/Hero';
 import Projects from '../components/Projects/Projects';
 import Technologies from '../components/Technologies/Technologies';
 import Timeline from '../components/TimeLine/TimeLine';
+//Imports the Layout and Section components
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
 
+//Defines a function component  Home that returns a Layout component with several child components.
 const Home = () => {
 	return (
 		<Layout>
@@ -22,4 +25,5 @@ const Home = () => {
 	);
 };
 
+//exports the Home component
 export default Home;
