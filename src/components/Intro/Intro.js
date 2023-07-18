70d12f28d1bdaa9a32ce208f6c2dfa05f0e9a205
yesayajones <yesayajones@gmail.com>
2023-07-18 18:16:11 +0200

    chore: Update hero section

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -7,33 +7,121 @@ import {
 	SectionTitle,
 } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection, RightSection } from './IntroStyles';
+import {
+	LeftSection,
+	RightSection,
+	Container,
+	Content,
+	LeftContent,
+	Title,
+	Text,
+	Name,
+	Jacob,
+	RightContent,
+	Buttonn,
+	ButtonText,
+	ButtonIcon,
+	Icon,
+	IconBackground,
+	IconImage,
+	IconImageGradient,
+} from './IntroStyles';
 import { FaReact } from 'react-icons/fa';
-// import profile from '/public/images/background.png';
-
-// const Image = styled.img`
-// 	max-width: 100%;
-// 	height: auto;
-// `;
 
 const Intro = (props) => (
 	<Section row nopadding>
 		<LeftSection>
-			<SectionTitle main center>
-				Hi I'm <br />
-				Yesaya Jones
-			</SectionTitle>
-			<SectionText>
-				I am a front-end web developer with React experience based in Zimbabwe.
-				I offer different front-end development services.
-			</SectionText>
-			<Button onClick={() => (window.location = 'https://google.com')}>
-				Hire Me
-			</Button>
+			<LeftContent>
+				<Title>My name is</Title>
+				<Name>
+					Jacob
+					<br />
+					Jones...
+				</Name>
+				<Text>Product Designer</Text>
+				<Text>based in London</Text>
+			</LeftContent>
+			<RightContent>
+				<Buttonn>
+					<ButtonText>Let's talk</ButtonText>
+					<ButtonIcon>
+						<Icon>
+							<IconBackground />
+							<IconImage>
+								<IconImageGradient />
+							</IconImage>
+						</Icon>
+					</ButtonIcon>
+				</Buttonn>
+				<Text>+77 022 444 05 05</Text>
+				<Text>jacob360@gmail.com</Text>
+			</RightContent>
 		</LeftSection>
 		<RightSection>
-			<FaReact size={100} color='#000' />
-			{/* <ReactImage src={profile} /> */}
+			<div style={{ width: '100%', height: '100%', position: 'relative' }}>
+				<div
+					style={{
+						width: 403.06,
+						height: 81.17,
+						left: 268.74,
+						top: 678.04,
+						position: 'absolute',
+						transform: 'rotate(-29.96deg)',
+						transformOrigin: '0 0',
+						background:
+							'linear-gradient(270deg, #FFB147 0%, #FF6C63 52%, #B86ADF 100%)',
+					}}
+				></div>
+				<div
+					style={{
+						width: 452.01,
+						height: 109.26,
+						left: 0,
+						top: 261.23,
+						position: 'absolute',
+						transform: 'rotate(-29.96deg)',
+						transformOrigin: '0 0',
+						background:
+							'linear-gradient(270deg, #FFB147 0%, #FF6C63 52%, #B86ADF 100%)',
+					}}
+				></div>
+				<img
+					style={{
+						width: 528.15,
+						height: 691.29,
+						left: 75.87,
+						top: 0,
+						position: 'absolute',
+					}}
+					src='/images/yesaya.png'
+				/>
+				<div
+					style={{
+						width: 576.99,
+						height: 178.98,
+						left: 70.49,
+						top: 346.72,
+						position: 'absolute',
+						transform: 'rotate(-29.96deg)',
+						transformOrigin: '0 0',
+						background:
+							'linear-gradient(270deg, #FF6C63 0%, rgba(255, 177, 71, 0.20) 52%)',
+					}}
+				></div>
+				<div
+					style={{
+						width: 576.99,
+						height: 207.08,
+						left: 40.22,
+						top: 570.74,
+						position: 'absolute',
+						transform: 'rotate(-29.96deg)',
+						transformOrigin: '0 0',
+						background:
+							'linear-gradient(270deg, #B238EF 0%, rgba(255, 177, 71, 0.33) 100%)',
+					}}
+				></div>
+			</div>
 		</RightSection>
 	</Section>
 );
--- src/components/Intro/IntroStyles.js
+++ src/components/Intro/IntroStyles.js
@@ -29,3 +29,154 @@ export const RightSection = styled.div`
 	z-index: 1;
 	background-color: #fff;
 `;
+
+// Including the styling for the new left section
+
+export const Container = styled.div`
+	width: 100%;
+	height: 100%;
+	position: relative;
+`;
+
+export const Content = styled.div`
+	width: 545px;
+	height: 526px;
+	left: 52px;
+	top: 12px;
+	position: absolute;
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	align-items: flex-start;
+	gap: 64px;
+	z-index: 2;
+`;
+
+export const LeftContent = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	align-items: flex-start;
+	gap: 48px;
+`;
+
+export const Title = styled.span`
+	color: '#141313';
+	font-size: 70px;
+	font-family: 'IBM Plex Sans';
+	font-weight: 400;
+	text-transform: uppercase;
+	line-height: 90px;
+	word-wrap: break-word;
+`;
+
+export const Name = styled.span`
+	color: '#141313';
+	font-size: 102px;
+	font-family: 'IBM Plex Sans';
+	font-weight: 500;
+	text-transform: uppercase;
+	line-height: 90px;
+	word-wrap: break-word;
+`;
+
+export const Jacob = styled.span`
+	color: '#141313';
+	font-size: 102px;
+	font-family: 'IBM Plex Sans';
+	font-weight: 700;
+	text-transform: uppercase;
+	line-height: 90px;
+	word-wrap: break-word;
+`;
+
+export const RightContent = styled.div`
+	display: flex;
+	justify-content: flex-start;
+	align-items: flex-start;
+	gap: 12px;
+`;
+
+export const Icon = styled.div`
+	width: 32px;
+	height: 32px;
+	position: relative;
+`;
+
+export const IconBackground = styled.div`
+	width: 32px;
+	height: 32px;
+	left: 0;
+	top: 0;
+	position: absolute;
+	background: 'rgba(20, 19, 19, 0.05)';
+	border-radius: 9999px;
+`;
+
+export const IconImage = styled.div`
+	width: 20.65px;
+	height: 20.65px;
+	left: 6.19px;
+	top: 6.19px;
+	position: absolute;
+`;
+
+export const IconImageGradient = styled.div`
+	width: 17.2px;
+	height: 7.74px;
+	left: 6.98px;
+	top: 1.07px;
+	position: absolute;
+	transform: rotate(45deg);
+	transform-origin: 0 0;
+	background: '#141313';
+`;
+
+export const IconImageSolid = styled.div`
+	width: 17.2px;
+	height: 13.76px;
+	left: 1.72px;
+	top: 3.44px;
+	position: absolute;
+	background: '#141313';
+`;
+
+export const Text = styled.div`
+	color: '#141313';
+	font-size: 32px;
+	font-family: 'IBM Plex Sans';
+	font-style: italic;
+	font-weight: 700;
+	line-height: 32px;
+	word-wrap: break-word;
+`;
+
+export const Buttonn = styled.div`
+	align-self: stretch;
+	height: 56px;
+	padding-left: 40px;
+	padding-right: 40px;
+	padding-top: 18px;
+	padding-bottom: 18px;
+	background: '#141313';
+	justify-content: center;
+	align-items: center;
+	gap: 8px;
+	display: flex;
+`;
+
+export const ButtonText = styled.div`
+	color: '#FFFFFF';
+	font-size: 16px;
+	font-family: 'IBM Plex Sans';
+	font-weight: 500;
+	text-align: center;
+	text-transform: uppercase;
+	line-height: 16px;
+`;
+
+export const ButtonIcon = styled.div`
+	width: 32px;
+	height: 32px;
+	position: relative;
+`;
