ed1ea764dbcd9c8988cfe630d9ac09de9f99e22d
Yesaya Jones <yesayajones@gmail.com>
2023-08-16 10:59:00 +0200

    chore: Convert TimeLine code to Styled Components

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -1,112 +1,43 @@
-import React, { useState } from 'react';
-import { Card, CardContent, Typography } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import { motion } from 'framer-motion';
+import React from "react";
+import { BoxWrapper, AboutMe, Group, Timeline, OverlapGroup, Rectangle, Ellipse, Div, Ellipse2, Ellipse3, TextWrapper, TextWrapper2, TextWrapper3, TextWrapper4, P, TextWrapper5, TextWrapper6, TextWrapper7, TextWrapper8 } from "./TimeLineStyles.js";
 
-const useStyles = makeStyles((theme) => ({
-	root: {
-		display: 'flex',
-		flexDirection: 'column',
-		alignItems: 'center',
-		padding: theme.spacing(2),
-	},
-	card: {
-		marginBottom: theme.spacing(2),
-		width: '100%',
-		maxWidth: '500px',
-		backgroundColor: '#fff',
-		boxShadow:
-			'0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)',
-	},
-	title: {
-		fontWeight: 'bold',
-		fontSize: '24px',
-		lineHeight: '32px',
-		letterSpacing: '0.02em',
-		color: '#000',
-		marginBottom: '8px',
-	},
-	subtitle: {
-		fontSize: '16px',
-		lineHeight: '24px',
-		letterSpacing: '0.02em',
-		color: '#424242',
-		marginBottom: '8px',
-	},
-	text: {
-		fontSize: '14px',
-		lineHeight: '22px',
-		letterSpacing: '0.02em',
-		color: '#424242',
-	},
-}));
-
-const TimeLine = () => {
-	const classes = useStyles();
-	const [activeStep, setActiveStep] = useState(0);
-
-	const handleStepClick = (step) => {
-		setActiveStep(step);
-	};
-
-	return (
-		<div className={classes.root}>
-			<motion.div
-				animate={{ opacity: 1 }}
-				initial={{ opacity: 0 }}
-				transition={{ duration: 0.6 }}
-			>
-				<Card className={classes.card}>
-					<CardContent>
-						<Typography className={classes.title}>Software Engineer</Typography>
-						<Typography className={classes.subtitle}>2018 - Present</Typography>
-						<Typography className={classes.text}>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-							euismod, sapien vel bibendum bibendum, velit sapien bibendum
-							sapien, vel bibendum sapien sapien vel bibendum bibendum, velit
-							sapien bibendum sapien, vel bibendum sapien.
-						</Typography>
-					</CardContent>
-				</Card>
-			</motion.div>
-			<motion.div
-				animate={{ opacity: 1 }}
-				initial={{ opacity: 0 }}
-				transition={{ duration: 0.6, delay: 0.2 }}
-			>
-				<Card className={classes.card}>
-					<CardContent>
-						<Typography className={classes.title}>Graphic Designer</Typography>
-						<Typography className={classes.subtitle}>2016 - 2018</Typography>
-						<Typography className={classes.text}>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-							euismod, sapien vel bibendum bibendum, velit sapien bibendum
-							sapien, vel bibendum sapien sapien vel bibendum bibendum, velit
-							sapien bibendum sapien, vel bibendum sapien.
-						</Typography>
-					</CardContent>
-				</Card>
-			</motion.div>
-			<motion.div
-				animate={{ opacity: 1 }}
-				initial={{ opacity: 0 }}
-				transition={{ duration: 0.6, delay: 0.4 }}
-			>
-				<Card className={classes.card}>
-					<CardContent>
-						<Typography className={classes.title}>Web Developer</Typography>
-						<Typography className={classes.subtitle}>2014 - 2016</Typography>
-						<Typography className={classes.text}>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-							euismod, sapien vel bibendum bibendum, velit sapien bibendum
-							sapien, vel bibendum sapien sapien vel bibendum bibendum, velit
-							sapien bibendum sapien, vel bibendum sapien.
-						</Typography>
-					</CardContent>
-				</Card>
-			</motion.div>
-		</div>
-	);
+export const Box = () => {
+  return (
+    <BoxWrapper>
+      <AboutMe>
+        <Group>
+          <Timeline>
+            <OverlapGroup>
+              <Rectangle />
+              <Ellipse />
+              <Div />
+              <Ellipse2 />
+              <Ellipse3 />
+            </OverlapGroup>
+          </Timeline>
+          <TextWrapper>2018</TextWrapper>
+          <TextWrapper2>2019</TextWrapper2>
+          <TextWrapper3>2020</TextWrapper3>
+          <TextWrapper4>2021</TextWrapper4>
+          <P>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </P>
+          <TextWrapper5>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </TextWrapper5>
+          <TextWrapper6>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </TextWrapper6>
+          <TextWrapper7>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </TextWrapper7>
+        </Group>
+        <TextWrapper8>About me</TextWrapper8>
+      </AboutMe>
+    </BoxWrapper>
+  );
 };
-
-export default TimeLine;
